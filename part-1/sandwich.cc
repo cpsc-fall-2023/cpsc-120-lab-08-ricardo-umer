5c08bdf7b0418d009c16447b15d924d8ded496ae
Umer Anwar <UmerAnwar@csu.fullerton.edu>
2023-10-30 14:20:37 -0700

    Made changes to file and used make commands to make sure it functions.

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -11,12 +11,11 @@ int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
   if (arguments.size() != 4) {
-    std::cout << "error: you must supply three arguments" 
+    std::cout << "error: you must supply three arguments"
               << ".\n";
     return 1;
   }
 
-  std::string sandich{arguments.at(0)};
   std::string protein{arguments.at(1)};
   std::string bread{arguments.at(2)};
   std::string condiment{arguments.at(3)};
